322810634798180ef9a2b56e1c1c4415e13ceb53
Xia0JianZheng <zhengx@elpuig.xeill.net>
2023-05-25 20:18:50 +0200

    shop(script1.0)

--- Global.gd
+++ Global.gd
@@ -3,3 +3,22 @@ extends Node
 var score = 0
 
 var gold = 0
+
+var shop
+var save_shop_path = 'res://save/shop'
+
+func save_shop():
+	var file = File.new()
+	file.open(save_shop_path, file.WRITE_READ)
+	file.store_var(shop)
+	file.close()
+
+func load_shop():
+	var file = File.new()
+	if not file.file_exists(save_shop_path):
+		return false
+	file.open(save_shop_path, file.READ)
+	shop = file.get_var()
+	file.close()
+	return true
+
