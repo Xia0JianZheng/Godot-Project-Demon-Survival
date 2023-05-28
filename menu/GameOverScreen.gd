42fa1879c7e67f74c3a2b73068d6eae4a404298e
Xia0JianZheng <zhengx@elpuig.xeill.net>
2023-05-28 16:58:35 +0200

    change game over screen

--- menu/GameOverScreen.gd
+++ menu/GameOverScreen.gd
@@ -1,22 +1,22 @@
 extends Control
 
+func _onready():
+	$GameOverMenu/Submit.disabled = false
+
 func _on_Main_Menu_pressed():
 	get_tree().change_scene("res://menu/Menu.tscn")
-
-
-func _on_Play_Again_pressed():
-	get_tree().change_scene("res://level/level.tscn")
-
 
 func _on_Shop_pressed():
 	get_tree().change_scene("res://Shop/Shop.tscn")
 
 func _on_Submit_pressed():
+	$PlayerName.text = ""
 	var global = get_node("/root/Global") 
 	if global != null:
 		print("saving Score")
 		global.save_name()
 		global.save_score()
+		$GameOverMenu/Submit.disabled = true
 
 func _on_GameOverMenu_ready():
-	$GameOverMenu/PlayAgain.grab_focus()
+	$GameOverMenu/Submit.grab_focus()
